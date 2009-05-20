f43ab619fc8a072b25903ef1ae31085993b8e697
Ned Batchelder <ned@nedbatchelder.com>
2009-05-20 06:47:12 -0400

    Automate testing on a number of Pythons.

---
+++ alltests.cmd
@@ -0,0 +1,12 @@
+call \ned\bin\switchpy 23
+python setup.py -q install
+python scripts/test_cog.py
+call \ned\bin\switchpy 24
+python setup.py -q install
+python scripts/test_cog.py
+call \ned\bin\switchpy 25
+python setup.py -q install
+python scripts/test_cog.py
+call \ned\bin\switchpy 26
+python setup.py -q install
+python scripts/test_cog.py
