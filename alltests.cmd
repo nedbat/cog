491ee4b22d355df22dcc6b34aa43694d7ebbee7d
Ned Batchelder <ned@nedbatchelder.com>
2009-05-20 07:43:42 -0400

    Add Jython to the menagerie of test environments.

--- alltests.cmd
+++ alltests.cmd
@@ -10,3 +10,6 @@ python scripts/test_cog.py
 call \ned\bin\switchpy 26
 python setup.py -q install
 python scripts/test_cog.py
+call \ned\bin\switchjy 2.5b4
+call c:\app\jython2.5b4\jython setup.py -q install
+call c:\app\jython2.5b4\jython scripts/test_cog.py
