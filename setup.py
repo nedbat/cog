98ca00b33db4148ae53c774c6e39431649053690
Ned Batchelder <ned@nedbatchelder.com>
2016-02-13 08:22:05 -0500

    Add 3.5 testing, and be clear that it's supported

--- setup.py
+++ setup.py
@@ -44,8 +44,11 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Topic :: Software Development :: Code Generators",
         ],
 
