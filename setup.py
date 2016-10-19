665a434c51bace488abe6468dc9e31da1a2a4cd0
Ned Batchelder <ned@nedbatchelder.com>
2016-10-19 19:19:52 -0400

    Add a license

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ else:
 
 setup(
     name = 'cogapp',    # Because there's already a Cog in pypi!  :(
-    version = '2.5',
+    version = '2.5.1',
     url = 'http://nedbatchelder.com/code/cog',
     author = 'Ned Batchelder',
     author_email = 'ned@nedbatchelder.com',
