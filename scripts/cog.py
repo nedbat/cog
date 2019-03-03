e4322b567e211eb1ca4fb9f63cb4bf233b4b0cd8
Ned Batchelder <ned@nedbatchelder.com>
2019-03-02 19:27:55 -0500

    No need to measure time. Fixes #14

--- scripts/cog.py
+++ scripts/cog.py
@@ -2,16 +2,10 @@
 """ Cog code generation tool.
     http://nedbatchelder.com/code/cog
 
-    Copyright 2004-2015, Ned Batchelder.
+    Copyright 2004-2019, Ned Batchelder.
 """
-
-import time
-start = time.clock()
 
 import sys
 from cogapp import Cog
 
-ret = Cog().main(sys.argv)
-
-#print "Time: %.2f sec" % (time.clock() - start)
-sys.exit(ret)
+sys.exit(Cog().main(sys.argv))
