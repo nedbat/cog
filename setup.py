8635716c224eacb20fef25e661717f1de33b09a7
Ned Batchelder <ned@nedbatchelder.com>
2019-03-08 07:36:36 -0500

    Bring details up to date

--- setup.py
+++ setup.py
@@ -9,18 +9,18 @@ from setuptools import setup
 
 setup(
     name = 'cogapp',    # Because there's already a Cog in pypi!  :(
-    version = '2.5.1',
+    version = '3.0.0',
     url = 'http://nedbatchelder.com/code/cog',
     author = 'Ned Batchelder',
     author_email = 'ned@nedbatchelder.com',
     description =
-        'Cog: A code generator for executing Python snippets in source files.',
+        'Cog: A content generator for executing Python snippets in source files.',
 
     long_description = '''\
         Docs at `http://nedbatchelder.com/code/cog <http://nedbatchelder.com/code/cog>`_.
 
         Code repository and issue tracker are at
-        `bitbucket.org <http://bitbucket.org/ned/cog>`_.
+        `GitHub <https://github.com/nedbat/cog`_.
         ''',
 
     classifiers = [
@@ -33,8 +33,12 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Code Generators",
         ],
+
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4",
 
     license = 'MIT',
 
