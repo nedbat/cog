477d548329529cca0e7b6b67d61943b0124b0607
Ned Batchelder <ned@nedbatchelder.com>
2019-03-10 15:04:13 -0400

    Drop Python 3.4

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
@@ -38,7 +37,7 @@ setup(
         "Topic :: Software Development :: Code Generators",
         ],
 
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4",
 
     license = 'MIT',
 
