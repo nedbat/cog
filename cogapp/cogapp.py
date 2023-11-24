08289eb874ad11a33289f4292b7f5e3c1a73f947
Ned Batchelder <ned@nedbatchelder.com>
2023-11-24 07:35:21 -0500

    refactor: move utilities to utils.py

--- cogapp/cogapp.py
+++ cogapp/cogapp.py
@@ -2,10 +2,8 @@
 """
 
 import copy
-import functools
 import getopt
 import glob
-import hashlib
 import io
 import linecache
 import os
@@ -16,6 +14,7 @@ import traceback
 import types
 
 from .whiteutils import commonPrefix, reindentBlock, whitePrefix
+from .utils import NumberedFileReader, Redirectable, md5
 
 __version__ = "4.0.0.dev2"
 
@@ -62,13 +61,6 @@ OPTIONS:
     -h          Print this help.
 """
 
-# Support FIPS mode where possible (Python >= 3.9). We don't use MD5 for security.
-md5 = (
-    functools.partial(hashlib.md5, usedforsecurity=False)
-    if sys.version_info >= (3, 9)
-    else hashlib.md5
-)
-
 class CogError(Exception):
     """ Any exception raised by Cog.
     """
@@ -103,27 +95,6 @@ class CogCheckFailed(CogError):
     """ A --check failed.
     """
     pass
-
-class Redirectable:
-    """ An object with its own stdout and stderr files.
-    """
-    def __init__(self):
-        self.stdout = sys.stdout
-        self.stderr = sys.stderr
-
-    def setOutput(self, stdout=None, stderr=None):
-        """ Assign new files for standard out and/or standard error.
-        """
-        if stdout:
-            self.stdout = stdout
-        if stderr:
-            self.stderr = stderr
-
-    def prout(self, s, end="\n"):
-        print(s, file=self.stdout, end=end)
-
-    def prerr(self, s, end="\n"):
-        print(s, file=self.stderr, end=end)
 
 
 class CogGenerator(Redirectable):
@@ -233,23 +204,6 @@ class CogGenerator(Redirectable):
             traceback.
         """
         raise CogGeneratedError(msg)
-
-
-class NumberedFileReader:
-    """ A decorator for files that counts the readline()'s called.
-    """
-    def __init__(self, f):
-        self.f = f
-        self.n = 0
-
-    def readline(self):
-        l = self.f.readline()
-        if l:
-            self.n += 1
-        return l
-
-    def linenumber(self):
-        return self.n
 
 
 class CogOptions:
---
+++ cogapp/utils.py
@@ -0,0 +1,53 @@
+""" Utilities for cog.
+"""
+
+import functools
+import hashlib
+import sys
+
+
+# Support FIPS mode where possible (Python >= 3.9). We don't use MD5 for security.
+md5 = (
+    functools.partial(hashlib.md5, usedforsecurity=False)
+    if sys.version_info >= (3, 9)
+    else hashlib.md5
+)
+
+
+class Redirectable:
+    """ An object with its own stdout and stderr files.
+    """
+    def __init__(self):
+        self.stdout = sys.stdout
+        self.stderr = sys.stderr
+
+    def setOutput(self, stdout=None, stderr=None):
+        """ Assign new files for standard out and/or standard error.
+        """
+        if stdout:
+            self.stdout = stdout
+        if stderr:
+            self.stderr = stderr
+
+    def prout(self, s, end="\n"):
+        print(s, file=self.stdout, end=end)
+
+    def prerr(self, s, end="\n"):
+        print(s, file=self.stderr, end=end)
+
+
+class NumberedFileReader:
+    """ A decorator for files that counts the readline()'s called.
+    """
+    def __init__(self, f):
+        self.f = f
+        self.n = 0
+
+    def readline(self):
+        l = self.f.readline()
+        if l:
+            self.n += 1
+        return l
+
+    def linenumber(self):
+        return self.n
