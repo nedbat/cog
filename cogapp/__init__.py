e591c4d3557974f1aa41070c60f1c95e2f949b75
Tim Vergenz <vergenzt@gmail.com>
2024-04-24 22:12:58 -0400

    refactor: reformat Python code via ruff

--- cogapp/__init__.py
+++ cogapp/__init__.py
@@ -1,7 +1,7 @@
-""" Cog content generation tool.
-    http://nedbatchelder.com/code/cog
+"""Cog content generation tool.
+http://nedbatchelder.com/code/cog
 
-    Copyright 2004-2024, Ned Batchelder.
+Copyright 2004-2024, Ned Batchelder.
 """
 
 from .cogapp import Cog, CogUsageError, main
--- cogapp/test_whiteutils.py
+++ cogapp/test_whiteutils.py
@@ -1,5 +1,4 @@
-""" Test the cogapp.whiteutils module.
-"""
+"""Test the cogapp.whiteutils module."""
 
 from unittest import TestCase
 
@@ -7,91 +6,91 @@ from .whiteutils import commonPrefix, reindentBlock, whitePrefix
 
 
 class WhitePrefixTests(TestCase):
-    """ Test cases for cogapp.whiteutils.
-    """
+    """Test cases for cogapp.whiteutils."""
+
     def testSingleLine(self):
-        self.assertEqual(whitePrefix(['']), '')
-        self.assertEqual(whitePrefix([' ']), '')
-        self.assertEqual(whitePrefix(['x']), '')
-        self.assertEqual(whitePrefix([' x']), ' ')
-        self.assertEqual(whitePrefix(['\tx']), '\t')
-        self.assertEqual(whitePrefix(['  x']), '  ')
-        self.assertEqual(whitePrefix([' \t \tx   ']), ' \t \t')
+        self.assertEqual(whitePrefix([""]), "")
+        self.assertEqual(whitePrefix([" "]), "")
+        self.assertEqual(whitePrefix(["x"]), "")
+        self.assertEqual(whitePrefix([" x"]), " ")
+        self.assertEqual(whitePrefix(["\tx"]), "\t")
+        self.assertEqual(whitePrefix(["  x"]), "  ")
+        self.assertEqual(whitePrefix([" \t \tx   "]), " \t \t")
 
     def testMultiLine(self):
-        self.assertEqual(whitePrefix(['  x','  x','  x']), '  ')
-        self.assertEqual(whitePrefix(['   y','  y',' y']), ' ')
-        self.assertEqual(whitePrefix([' y','  y','   y']), ' ')
+        self.assertEqual(whitePrefix(["  x", "  x", "  x"]), "  ")
+        self.assertEqual(whitePrefix(["   y", "  y", " y"]), " ")
+        self.assertEqual(whitePrefix([" y", "  y", "   y"]), " ")
 
     def testBlankLinesAreIgnored(self):
-        self.assertEqual(whitePrefix(['  x','  x','','  x']), '  ')
-        self.assertEqual(whitePrefix(['','  x','  x','  x']), '  ')
-        self.assertEqual(whitePrefix(['  x','  x','  x','']), '  ')
-        self.assertEqual(whitePrefix(['  x','  x','          ','  x']), '  ')
+        self.assertEqual(whitePrefix(["  x", "  x", "", "  x"]), "  ")
+        self.assertEqual(whitePrefix(["", "  x", "  x", "  x"]), "  ")
+        self.assertEqual(whitePrefix(["  x", "  x", "  x", ""]), "  ")
+        self.assertEqual(whitePrefix(["  x", "  x", "          ", "  x"]), "  ")
 
     def testTabCharacters(self):
-        self.assertEqual(whitePrefix(['\timport sys', '', '\tprint sys.argv']), '\t')
+        self.assertEqual(whitePrefix(["\timport sys", "", "\tprint sys.argv"]), "\t")
 
     def testDecreasingLengths(self):
-        self.assertEqual(whitePrefix(['   x','  x',' x']), ' ')
-        self.assertEqual(whitePrefix(['     x',' x',' x']), ' ')
+        self.assertEqual(whitePrefix(["   x", "  x", " x"]), " ")
+        self.assertEqual(whitePrefix(["     x", " x", " x"]), " ")
 
 
 class ReindentBlockTests(TestCase):
-    """ Test cases for cogapp.reindentBlock.
-    """
+    """Test cases for cogapp.reindentBlock."""
+
     def testNonTermLine(self):
-        self.assertEqual(reindentBlock(''), '')
-        self.assertEqual(reindentBlock('x'), 'x')
-        self.assertEqual(reindentBlock(' x'), 'x')
-        self.assertEqual(reindentBlock('  x'), 'x')
-        self.assertEqual(reindentBlock('\tx'), 'x')
-        self.assertEqual(reindentBlock('x', ' '), ' x')
-        self.assertEqual(reindentBlock('x', '\t'), '\tx')
-        self.assertEqual(reindentBlock(' x', ' '), ' x')
-        self.assertEqual(reindentBlock(' x', '\t'), '\tx')
-        self.assertEqual(reindentBlock(' x', '  '), '  x')
+        self.assertEqual(reindentBlock(""), "")
+        self.assertEqual(reindentBlock("x"), "x")
+        self.assertEqual(reindentBlock(" x"), "x")
+        self.assertEqual(reindentBlock("  x"), "x")
+        self.assertEqual(reindentBlock("\tx"), "x")
+        self.assertEqual(reindentBlock("x", " "), " x")
+        self.assertEqual(reindentBlock("x", "\t"), "\tx")
+        self.assertEqual(reindentBlock(" x", " "), " x")
+        self.assertEqual(reindentBlock(" x", "\t"), "\tx")
+        self.assertEqual(reindentBlock(" x", "  "), "  x")
 
     def testSingleLine(self):
-        self.assertEqual(reindentBlock('\n'), '\n')
-        self.assertEqual(reindentBlock('x\n'), 'x\n')
-        self.assertEqual(reindentBlock(' x\n'), 'x\n')
-        self.assertEqual(reindentBlock('  x\n'), 'x\n')
-        self.assertEqual(reindentBlock('\tx\n'), 'x\n')
-        self.assertEqual(reindentBlock('x\n', ' '), ' x\n')
-        self.assertEqual(reindentBlock('x\n', '\t'), '\tx\n')
-        self.assertEqual(reindentBlock(' x\n', ' '), ' x\n')
-        self.assertEqual(reindentBlock(' x\n', '\t'), '\tx\n')
-        self.assertEqual(reindentBlock(' x\n', '  '), '  x\n')
+        self.assertEqual(reindentBlock("\n"), "\n")
+        self.assertEqual(reindentBlock("x\n"), "x\n")
+        self.assertEqual(reindentBlock(" x\n"), "x\n")
+        self.assertEqual(reindentBlock("  x\n"), "x\n")
+        self.assertEqual(reindentBlock("\tx\n"), "x\n")
+        self.assertEqual(reindentBlock("x\n", " "), " x\n")
+        self.assertEqual(reindentBlock("x\n", "\t"), "\tx\n")
+        self.assertEqual(reindentBlock(" x\n", " "), " x\n")
+        self.assertEqual(reindentBlock(" x\n", "\t"), "\tx\n")
+        self.assertEqual(reindentBlock(" x\n", "  "), "  x\n")
 
     def testRealBlock(self):
         self.assertEqual(
-            reindentBlock('\timport sys\n\n\tprint sys.argv\n'),
-            'import sys\n\nprint sys.argv\n'
-            )
+            reindentBlock("\timport sys\n\n\tprint sys.argv\n"),
+            "import sys\n\nprint sys.argv\n",
+        )
 
 
 class CommonPrefixTests(TestCase):
-    """ Test cases for cogapp.commonPrefix.
-    """
+    """Test cases for cogapp.commonPrefix."""
+
     def testDegenerateCases(self):
-        self.assertEqual(commonPrefix([]), '')
-        self.assertEqual(commonPrefix(['']), '')
-        self.assertEqual(commonPrefix(['','','','','']), '')
-        self.assertEqual(commonPrefix(['cat in the hat']), 'cat in the hat')
+        self.assertEqual(commonPrefix([]), "")
+        self.assertEqual(commonPrefix([""]), "")
+        self.assertEqual(commonPrefix(["", "", "", "", ""]), "")
+        self.assertEqual(commonPrefix(["cat in the hat"]), "cat in the hat")
 
     def testNoCommonPrefix(self):
-        self.assertEqual(commonPrefix(['a','b']), '')
-        self.assertEqual(commonPrefix(['a','b','c','d','e','f']), '')
-        self.assertEqual(commonPrefix(['a','a','a','a','a','x']), '')
+        self.assertEqual(commonPrefix(["a", "b"]), "")
+        self.assertEqual(commonPrefix(["a", "b", "c", "d", "e", "f"]), "")
+        self.assertEqual(commonPrefix(["a", "a", "a", "a", "a", "x"]), "")
 
     def testUsualCases(self):
-        self.assertEqual(commonPrefix(['ab', 'ac']), 'a')
-        self.assertEqual(commonPrefix(['aab', 'aac']), 'aa')
-        self.assertEqual(commonPrefix(['aab', 'aab', 'aab', 'aac']), 'aa')
+        self.assertEqual(commonPrefix(["ab", "ac"]), "a")
+        self.assertEqual(commonPrefix(["aab", "aac"]), "aa")
+        self.assertEqual(commonPrefix(["aab", "aab", "aab", "aac"]), "aa")
 
     def testBlankLine(self):
-        self.assertEqual(commonPrefix(['abc', 'abx', '', 'aby']), '')
+        self.assertEqual(commonPrefix(["abc", "abx", "", "aby"]), "")
 
     def testDecreasingLengths(self):
-        self.assertEqual(commonPrefix(['abcd', 'abc', 'ab']), 'ab')
+        self.assertEqual(commonPrefix(["abcd", "abc", "ab"]), "ab")
