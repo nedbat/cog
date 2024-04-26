fc5d3bba8b458ec8b73f4174ad9ba253751e2758
Tim Vergenz <vergenzt@gmail.com>
2024-04-26 19:18:17 -0400

    refactor: convert camelCase to snake_case
    Purposes:
    1. To match typical modern Python coding styles (recommended by PEP 8)
    2. To prepare for argparse usage, which uses snake_case for default dests
    
    Via https://ast-grep.github.io/, with rule:
    
    ```
    id: _
    language: python
    
    rule:
      pattern: $ID
      kind: identifier
      all:
      - regex: '^[^A-Z]'  # not PascalCase
      - regex: '[a-z][A-Z]'  # has camelCase transition
    
      not:
        any:
        # backcompat (part of cog module API)
        - regex: ^sOut$
        - inside:
            kind: attribute
            has:
              field: object
              regex: \.cogmodule$
    
        # unittest methods
        - regex: ^assert
        - regex: ^setUp$
        - regex: ^tearDown$
        - regex: ^addCleanup$
    
    transform:
      ID_SNAKE:
        convert:
          source: $ID
          toCase: snakeCase
      UNDERSCORE:
        replace:
          source: $ID
          replace: '^(_)?.*'
          by: '$1'
    
    fix: $UNDERSCORE$ID_SNAKE
    ```
    
    plus manual review of all changes

--- cogapp/test_makefiles.py
+++ cogapp/test_makefiles.py
@@ -23,32 +23,32 @@ class SimpleTests(TestCase):
     def exists(self, dname, fname):
         return os.path.exists(os.path.join(dname, fname))
 
-    def checkFilesExist(self, d, dname):
+    def check_files_exist(self, d, dname):
         for fname in d.keys():
             assert self.exists(dname, fname)
             if isinstance(d[fname], dict):
-                self.checkFilesExist(d[fname], os.path.join(dname, fname))
+                self.check_files_exist(d[fname], os.path.join(dname, fname))
 
-    def checkFilesDontExist(self, d, dname):
+    def check_files_dont_exist(self, d, dname):
         for fname in d.keys():
             assert not self.exists(dname, fname)
 
-    def testOneFile(self):
+    def test_one_file(self):
         fname = "foo.txt"
         notfname = "not_here.txt"
         d = {fname: "howdy"}
         assert not self.exists(self.tempdir, fname)
         assert not self.exists(self.tempdir, notfname)
 
-        makefiles.makeFiles(d, self.tempdir)
+        makefiles.make_files(d, self.tempdir)
         assert self.exists(self.tempdir, fname)
         assert not self.exists(self.tempdir, notfname)
 
-        makefiles.removeFiles(d, self.tempdir)
+        makefiles.remove_files(d, self.tempdir)
         assert not self.exists(self.tempdir, fname)
         assert not self.exists(self.tempdir, notfname)
 
-    def testManyFiles(self):
+    def test_many_files(self):
         d = {
             "top1.txt": "howdy",
             "top2.txt": "hello",
@@ -58,13 +58,13 @@ class SimpleTests(TestCase):
             },
         }
 
-        self.checkFilesDontExist(d, self.tempdir)
-        makefiles.makeFiles(d, self.tempdir)
-        self.checkFilesExist(d, self.tempdir)
-        makefiles.removeFiles(d, self.tempdir)
-        self.checkFilesDontExist(d, self.tempdir)
+        self.check_files_dont_exist(d, self.tempdir)
+        makefiles.make_files(d, self.tempdir)
+        self.check_files_exist(d, self.tempdir)
+        makefiles.remove_files(d, self.tempdir)
+        self.check_files_dont_exist(d, self.tempdir)
 
-    def testOverlapping(self):
+    def test_overlapping(self):
         d1 = {
             "top1.txt": "howdy",
             "sub": {
@@ -79,26 +79,26 @@ class SimpleTests(TestCase):
             },
         }
 
-        self.checkFilesDontExist(d1, self.tempdir)
-        self.checkFilesDontExist(d2, self.tempdir)
-        makefiles.makeFiles(d1, self.tempdir)
-        makefiles.makeFiles(d2, self.tempdir)
-        self.checkFilesExist(d1, self.tempdir)
-        self.checkFilesExist(d2, self.tempdir)
-        makefiles.removeFiles(d1, self.tempdir)
-        makefiles.removeFiles(d2, self.tempdir)
-        self.checkFilesDontExist(d1, self.tempdir)
-        self.checkFilesDontExist(d2, self.tempdir)
+        self.check_files_dont_exist(d1, self.tempdir)
+        self.check_files_dont_exist(d2, self.tempdir)
+        makefiles.make_files(d1, self.tempdir)
+        makefiles.make_files(d2, self.tempdir)
+        self.check_files_exist(d1, self.tempdir)
+        self.check_files_exist(d2, self.tempdir)
+        makefiles.remove_files(d1, self.tempdir)
+        makefiles.remove_files(d2, self.tempdir)
+        self.check_files_dont_exist(d1, self.tempdir)
+        self.check_files_dont_exist(d2, self.tempdir)
 
-    def testContents(self):
+    def test_contents(self):
         fname = "bar.txt"
         cont0 = "I am bar.txt"
         d = {fname: cont0}
-        makefiles.makeFiles(d, self.tempdir)
+        makefiles.make_files(d, self.tempdir)
         with open(os.path.join(self.tempdir, fname)) as fcont1:
             assert fcont1.read() == cont0
 
-    def testDedent(self):
+    def test_dedent(self):
         fname = "dedent.txt"
         d = {
             fname: """\
@@ -108,7 +108,7 @@ class SimpleTests(TestCase):
                 OK.
                 """,
         }
-        makefiles.makeFiles(d, self.tempdir)
+        makefiles.make_files(d, self.tempdir)
         with open(os.path.join(self.tempdir, fname)) as fcont:
             assert (
                 fcont.read() == "This is dedent.txt\n\tTabbed in.\n  spaced in.\nOK.\n"
